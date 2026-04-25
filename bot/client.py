bf3a5229dd4470659eb9ddd75a141a85b3a65d84
tanishqsrivastavaa <tanishq280205@gmail.com>
2026-04-25 13:26:30 +0530

    Add Discord client implementation with message intents

---
+++ bot/client.py
@@ -0,0 +1,10 @@
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.messages = True
+
+
+def create_client():
+    client = discord.Client(intents = intents)
+    return client
